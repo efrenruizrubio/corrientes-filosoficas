a5a49f8c3308b368ba38e4907e0baec2300df627
efrenruizrubio <efren282@outlook.es>
2023-01-12 22:45:51 -0600

    :sparkles: [ADD]: Added more sections of answers

--- src/index.js
+++ src/index.js
@@ -1,169 +1,94 @@
-const movements = [
-	{
-		name: "Movement 1",
-		concept: {
-			name: "Concept 1",
-			id: "concept-1",
-			className: "concepts__container__item__text",
-		},
-		image: {
-			src: "/assets/images/therapy.jpg",
-			id: "image-1",
-			containerClassName: "images__container__item__inner-element",
-			className: "images__container__item__img",
-			width: "233",
-			height: "180",
-			alt: "imagen 1",
-		},
-		mainId: "1",
-	},
-	{
-		name: "Movement 2",
-		concept: {
-			name: "Concept 2",
-			id: "concept-2",
-			className: "concepts__container__item__text",
-		},
-		image: {
-			src: "/assets/images/therapy.jpg",
-			id: "image-2",
-			containerClassName: "images__container__item__inner-element",
-			className: "images__container__item__img",
-			width: "233",
-			height: "180",
-			alt: "imagen 2",
-		},
-		mainId: "2",
-	},
-	{
-		name: "Movement 3",
-		concept: {
-			name: "Concept 3",
-			id: "concept-3",
-			className: "concepts__container__item__text",
-		},
-		image: {
-			src: "/assets/images/therapy.jpg",
-			id: "image-3",
-			containerClassName: "images__container__item__inner-element",
-			className: "images__container__item__img",
-			width: "233",
-			height: "180",
-			alt: "imagen 3",
-		},
-		mainId: "3",
-	},
-	{
-		name: "Movement 4",
-		concept: {
-			name: "Concept 4",
-			id: "concept-4",
-			className: "concepts__container__item__text",
-		},
-		image: {
-			src: "/assets/images/therapy.jpg",
-			id: "image-4",
-			containerClassName: "images__container__item__inner-element",
-			className: "images__container__item__img",
-			width: "233",
-			height: "180",
-			alt: "imagen 4",
-		},
-		mainId: "4",
-	},
-	{
-		name: "Movement 5",
-		concept: {
-			name: "Concept 5",
-			id: "concept-5",
-			className: "concepts__container__item__text",
-		},
-		image: {
-			src: "/assets/images/therapy.jpg",
-			id: "image-5",
-			containerClassName: "images__container__item__inner-element",
-			className: "images__container__item__img",
-			width: "233",
-			height: "180",
-			alt: "imagen 5",
-		},
-		mainId: "5",
-	},
-	{
-		name: "Movement 6",
-		concept: {
-			name: "Concept 6",
-			id: "concept-6",
-			className: "concepts__container__item__text",
-		},
-		image: {
-			src: "/assets/images/therapy.jpg",
-			id: "image-6",
-			containerClassName: "images__container__item__inner-element",
-			className: "images__container__item__img",
-			width: "233",
-			height: "180",
-			alt: "imagen 6",
-		},
-		mainId: "6",
-	},
-];
+import { movements } from "./movements.js";
 
 const shuffleArray = (arr) => {
-	return arr.sort((a, b) => 0.5 - Math.random());
+	return arr.sort(() => 0.5 - Math.random());
 };
 
 const movementsContainer = document.querySelector("#movements");
 const conceptsContainer = document.querySelector("#concepts");
 const imagesContainer = document.querySelector("#images");
+const methodsContainer = document.querySelector("#methods");
+const thesisContainer = document.querySelector("#thesis");
 
 const conceptsArray = [];
 const imagesArray = [];
+const methodsArray = [];
+const thesisArray = [];
 
 const resetButton = document.querySelector(".reset-button");
 
 resetButton.disabled = true;
 
 const createElements = () => {
-	movements.forEach((movement, i) => {
+	movements.forEach((movement) => {
 		const itemContainer = document.createElement("div");
 		const innerItemContainer = document.createElement("div");
+
 		const innerConceptsContainer = document.createElement("div");
 		const innerImagesContainer = document.createElement("div");
+		const innerMethodsContainer = document.createElement("div");
+		const innerThesisContainer = document.createElement("div");
 
 		itemContainer.classList.add("movements__container__item");
 		innerItemContainer.classList.add("movements__container__item__container");
+
 		innerConceptsContainer.classList.add(
 			"movements__container__item__drop",
 			"movements__container__item__drop__concept",
 		);
-		innerConceptsContainer.id = `container-concept-${i + 1}`;
 		innerImagesContainer.classList.add(
 			"movements__container__item__drop",
 			"movements__container__item__drop__image",
 		);
-		innerImagesContainer.id = `container-image-${i + 1}`;
+		innerMethodsContainer.classList.add(
+			"movements__container__item__drop",
+			"movements__container__item__drop__method",
+		);
+		innerThesisContainer.classList.add(
+			"movements__container__item__drop",
+			"movements__container__item__drop__thesis",
+		);
+
+		innerConceptsContainer.id = `container-concept-${movement.mainId}`;
+		innerImagesContainer.id = `container-image-${movement.mainId}`;
+		innerMethodsContainer.id = `container-method-${movement.mainId}`;
+		innerThesisContainer.id = `container-thesis-${movement.mainId}`;
+
 		const title = document.createElement("h3");
+
 		const conceptSubtitle = document.createElement("h4");
 		const imageSubtitle = document.createElement("h4");
+		const methodSubtitle = document.createElement("h4");
+		const thesisSubtitle = document.createElement("h4");
 
 		title.classList.add("movements__title");
 		title.innerText = movement.name;
 
 		conceptSubtitle.classList.add("movements__subtitle", "concept__subtitle");
-		conceptSubtitle.innerText = "Concept";
 		imageSubtitle.classList.add("movements__subtitle", "image__subtitle");
-		imageSubtitle.innerText = "Image";
+		methodSubtitle.classList.add("movements__subtitle", "method__subtitle");
+		thesisSubtitle.classList.add("movements__subtitle", "thesis__subtitle");
+
+		conceptSubtitle.innerText = "Definición";
+		imageSubtitle.innerText = "Imagen";
+		methodSubtitle.innerText = "Método";
+		thesisSubtitle.innerText = "Tesis";
 
 		innerItemContainer.append(
 			conceptSubtitle,
 			imageSubtitle,
 			innerConceptsContainer,
 			innerImagesContainer,
+			methodSubtitle,
+			thesisSubtitle,
+			innerMethodsContainer,
+			innerThesisContainer,
 		);
 
 		conceptsArray.push(movement.concept);
 		imagesArray.push(movement.image);
+		methodsArray.push(movement.method);
+		thesisArray.push(movement.thesis);
 
 		itemContainer.append(title, innerItemContainer);
 		movementsContainer.append(itemContainer);
@@ -171,38 +96,65 @@ const createElements = () => {
 
 	const shuffledConcepts = shuffleArray(conceptsArray);
 	const shuffledImages = shuffleArray(imagesArray);
+	const shuffledMethods = shuffleArray(methodsArray);
+	const shuffledThesis = shuffleArray(thesisArray);
 
 	for (let i = 0; i < movements.length; i++) {
 		const conceptItemContainer = document.createElement("div");
 		const imageItemContainer = document.createElement("div");
 		const innerImageItemContainer = document.createElement("div");
+		const methodItemContainer = document.createElement("div");
+		const thesisItemContainer = document.createElement("div");
 
 		conceptItemContainer.classList.add("concepts__container__item");
-		conceptItemContainer.id = `concept-container-${shuffledConcepts[i].id}`;
-
 		imageItemContainer.classList.add("images__container__item");
+		methodItemContainer.classList.add("methods__container__item");
+		thesisItemContainer.classList.add("thesis__container__item");
+
+		conceptItemContainer.id = `concept-container-${shuffledConcepts[i].id}`;
 		imageItemContainer.id = `image-container-${shuffledImages[i].id}`;
+		methodItemContainer.id = `method-container-${shuffledMethods[i].id}`;
+		thesisItemContainer.id = `thesis-container-${shuffledThesis[i].id}`;
 
 		const concept = document.createElement("p");
 		const image = document.createElement("div");
+		const method = document.createElement("p");
+		const thesis = document.createElement("p");
 
 		concept.classList.add(shuffledConcepts[i].className);
+		image.classList.add(shuffledImages[i].className);
+		method.classList.add(shuffledMethods[i].className);
+		thesis.classList.add(shuffledThesis[i].className);
+
 		concept.innerText = shuffledConcepts[i].name;
 		concept.id = shuffledConcepts[i].id;
 		concept.draggable = true;
 
-		image.style.backgroundImage = `url(${shuffledImages[i].src})`;
-		image.classList.add(shuffledImages[i].className);
-
 		innerImageItemContainer.classList.add(shuffledImages[i].containerClassName);
 		innerImageItemContainer.id = shuffledImages[i].id;
 		innerImageItemContainer.draggable = true;
+
+		image.style.backgroundImage = `url(${shuffledImages[i].src})`;
 		innerImageItemContainer.append(image);
+
+		method.innerText = shuffledMethods[i].content;
+		method.id = shuffledMethods[i].id;
+		method.draggable = true;
+
+		thesis.innerText = shuffledThesis[i].content;
+		thesis.id = shuffledThesis[i].id;
+		thesis.draggable = true;
+
+		console.log(concept.offsetHeight);
 
 		conceptItemContainer.append(concept);
 		conceptsContainer.append(conceptItemContainer);
 		imageItemContainer.append(innerImageItemContainer);
 		imagesContainer.append(imageItemContainer);
+		methodItemContainer.append(method);
+		methodsContainer.append(methodItemContainer);
+		thesisItemContainer.append(thesis);
+		thesisContainer.append(thesisItemContainer);
 	}
 	if (window.innerWidth < 1024) {
 		const droppableContainer = document.createElement("div");
@@ -492,11 +444,15 @@ const submit = () => {
 	const correctAnswers = Array.from(document.querySelectorAll(".item-correct"));
 	const wrongAnswers = Array.from(document.querySelectorAll(".item-incorrect"));
 
+	const answersLength = answersArray.length;
+
 	Swal.fire({
 		title: "Resultado",
 		html: `<span class="alert__text">Obtuviste <strong class=alert__result${
-			correctAnswers.length > 6 ? "--approved" : "--failed"
-		}>${12 - wrongAnswers.length}</strong> respuesta(s) bien de 12</span>`,
+			correctAnswers.length > answersLength / 2 ? "--approved" : "--failed"
+		}>${
+			answersLength - wrongAnswers.length
+		}</strong> respuesta(s) bien de ${answersLength}.</span>`,
 		icon:
 			correctAnswers.length === 12
 				? "success"
@@ -525,12 +481,31 @@ const handleEvents = () => {
 		".movements__container__item__drop__image",
 	);
 
+	const methodsContainer = document.querySelectorAll(
+		".movements__container__item__drop__method",
+	);
+
+	const thesisContainer = document.querySelectorAll(
+		".movements__container__item__drop__thesis",
+	);
+
 	const concept = document.querySelectorAll(".concepts__container__item__text");
 	const image = document.querySelectorAll(
 		".images__container__item__inner-element",
 	);
+	const method = document.querySelectorAll(".methods__container__item__text");
+	const thesis = document.querySelectorAll(".thesis__container__item__text");
 
-	for (let i = 0; i < concept.length; i++) {
+	const titles = document.getElementsByClassName("section__title");
+
+	Array.from(titles).forEach((el) => {
+		el.addEventListener("click", () => {
+			const element = document.getElementById(`${el.id.split("__")[0]}`);
+			element.classList.toggle("section--hidden");
+		});
+	});
+
+	for (let i = 0; i < movements.length; i++) {
 		concept[i].addEventListener("dragstart", dragStart);
 		concept[i].addEventListener("dragend", dragEnd);
 		concept[i].removeEventListener("dragenter", dragEnter);
@@ -548,9 +523,21 @@ const handleEvents = () => {
 		image[i].addEventListener("contextmenu", (e) => {
 			e.preventDefault();
 		});
-	}
 
-	for (let i = 0; i < conceptsContainer.length; i++) {
+		method[i].addEventListener("dragstart", dragStart);
+		method[i].addEventListener("dragend", dragEnd);
+		method[i].removeEventListener("dragenter", dragEnter);
+		method[i].removeEventListener("dragleave", dragLeave);
+		method[i].removeEventListener("dragover", dragOver);
+		method[i].removeEventListener("drop", dragDrop);
+
+		thesis[i].addEventListener("dragstart", dragStart);
+		thesis[i].addEventListener("dragend", dragEnd);
+		thesis[i].removeEventListener("dragenter", dragEnter);
+		thesis[i].removeEventListener("dragleave", dragLeave);
+		thesis[i].removeEventListener("dragover", dragOver);
+		thesis[i].removeEventListener("drop", dragDrop);
+
 		conceptsContainer[i].addEventListener("dragover", dragOver);
 		conceptsContainer[i].addEventListener("dragenter", dragEnter);
 		conceptsContainer[i].addEventListener("dragleave", dragLeave);
@@ -560,6 +547,16 @@ const handleEvents = () => {
 		imagesContainer[i].addEventListener("dragenter", dragEnter);
 		imagesContainer[i].addEventListener("dragleave", dragLeave);
 		imagesContainer[i].addEventListener("drop", dragDrop);
+
+		methodsContainer[i].addEventListener("dragover", dragOver);
+		methodsContainer[i].addEventListener("dragenter", dragEnter);
+		methodsContainer[i].addEventListener("dragleave", dragLeave);
+		methodsContainer[i].addEventListener("drop", dragDrop);
+
+		thesisContainer[i].addEventListener("dragover", dragOver);
+		thesisContainer[i].addEventListener("dragenter", dragEnter);
+		thesisContainer[i].addEventListener("dragleave", dragLeave);
+		thesisContainer[i].addEventListener("drop", dragDrop);
 	}
 
 	if (window.innerWidth < 1024) {
---
+++ src/movements.js
@@ -0,0 +1,239 @@
+export const movements = [
+	{
+		name: "Fenomenología",
+		concept: {
+			name: "Análisis de la conciencia en su intencionalidad. Como la conciencia es intencionalidad en cuanto que siempre es conciencia de algo, el análisis de la misma es el análisis de todos los modos posibles en que algo puede ser dato para la conciencia (como percibido, pensado, recordado, simbolizado, amado, querido, etc.) y, por ello, de todo tipo de sentido o de validez que pueda ser reconocido en los objetos de conciencia.",
+			id: "concept-1",
+			className: "concepts__container__item__text",
+		},
+		image: {
+			src: "/assets/images/husserl.png",
+			id: "image-1",
+			containerClassName: "images__container__item__inner-element",
+			className: "images__container__item__img",
+			width: "200",
+			height: "200",
+			alt: "Imagen de Edmund Husserl",
+		},
+		method: {
+			content: "Es un metodo en si mismo",
+			id: "method-1",
+			className: "methods__container__item__text",
+		},
+		thesis: {
+			content:
+				"Es ciencia teorética (contemplativa) y rigurosa. Es ciencia intuitiva porque trata de captar esencias que se dan a la razón de un modo análogo a como se dan las cosas a la percepción sensible. Es ciencia no objetiva. Es ciencias de los orígenes y de los primeros principios. Es ciencia de la subjetividad. Es ciencia impersonal  “Porque sus colabores no tienen necesidad de sabiduría sino de dotes teoréticas.",
+			id: "thesis-1",
+			className: "thesis__container__item__text",
+		},
+		mainId: "1",
+	},
+	{
+		name: "Existencialismo",
+		concept: {
+			name: "Corriente filosófica que persigue el conocimiento de la realidad a través de la experiencia inmediata de la propia existencia. Este movimiento de la filosofía destaca al ser humano individual como creador del significado de su vida. La temporalidad del sujeto, su existencia concreta en el mundo, es aquello que constituye al ser y no una supuesta esencia más abstracta",
+			id: "concept-2",
+			className: "concepts__container__item__text",
+		},
+		image: {
+			src: "/assets/images/sartre-jaspers.png",
+			id: "image-2",
+			containerClassName: "images__container__item__inner-element",
+			className: "images__container__item__img",
+			width: "200",
+			height: "200",
+			alt: "Imagen de Jean Paul Sartre y Karl Jaspers",
+		},
+		method: {
+			content:
+				"El método fenomenológico. El psicoanálisis existencial. Método dialéctico",
+			id: "method-2",
+			className: "methods__container__item__text",
+		},
+		thesis: {
+			content:
+				"La existencia precede a la esencia. El hombre está condenado a ser libre. El hombre es un ser en situación. El existencialismo es humanismo (somos responsables de nosotros y de los demás). El hombre es una pasión inútil. Existen existencialismos ateos y cristianos, aunque por la propuesta en la ética es más congruente el existencialismo ateo.",
+			id: "thesis-2",
+			className: "thesis__container__item__text",
+		},
+		mainId: "2",
+	},
+	{
+		name: "Hermenéutica",
+		concept: {
+			name: "Teoría y práctica de la interpretación, y tiene un largo desarrollo histórico. Como disciplina moderna de la interpretación de textos, se formaliza en los siglos XVIII Y XIX, al final de este último, el filósofo Wilthelm Dithey la propone como metodología de las ciencias sociales. En el siglo XX, esta teoría sufre transformaciones en su campo y en su metodología, y se convierte en la base de un enfoque filosófico para el análisis de la comprensión y conducta humana.",
+			id: "concept-3",
+			className: "concepts__container__item__text",
+		},
+		image: {
+			src: "/assets/images/ricoeur-gadamer.png",
+			id: "image-3",
+			containerClassName: "images__container__item__inner-element",
+			className: "images__container__item__img",
+			width: "200",
+			height: "200",
+			alt: "Imagen de Paul Ricoeur y Hans-Georg Gadamer",
+		},
+		method: {
+			content:
+				"Es un metodo en si misma, pero por su propósito podemos hablar de tres tipos: La tradicional, la dialógica y la crítica.",
+			id: "method-3",
+			className: "methods__container__item__text",
+		},
+		thesis: {
+			content:
+				"El concepto de la verdad en el texto no necesariamente muestran correspondencia entre la comprensión del interprete y las intenciones del autor. La verdad del texto se concibe como una introspección reveladora. La investigación del contexto en el que se generó el texto ayuda a la comprensión del mismo, pero no define la interpretación. Existen muchas interpretaciones erróneas de un texto, pero también existen más de una interpretación correcta. Se considera que el significado siempre está condicionado y nunca pueda obtenerse totalmente. El círculo hermenéutico significa que el intérprete proyecta un significado en el texto y, a su vez, el texto lo confirma o lo rechaza. La H. Dialogica es principalmente descriptiva.Los diferentes puntos de vista pueden constituirse en la base de diferentes interpretaciones aceptables. El texto siempre rebasa a su autor.",
+			id: "thesis-3",
+			className: "thesis__container__item__text",
+		},
+		mainId: "3",
+	},
+	{
+		name: "Escuela de Frankfurt/Teoría crítica",
+		concept: {
+			name: "Su tarea primordial era especificar qué otro modelo de razón erap osible y si estaba capacitada para superar las alienaciones o esquizofrenias -como gustaban de llamarlas- de la sociedad capitalista del momento. Pero esto era una labor no sólo epistemológica, sino que desde la preocupación epistemológica por la ciencia tenía que incidir críticamente en la transformación de la sociedad de aquel momento. Su ideal es la emancipación social del individuo.",
+			id: "concept-4",
+			className: "concepts__container__item__text",
+		},
+		image: {
+			src: "/assets/images/marcuse-fromm.png",
+			id: "image-4",
+			containerClassName: "images__container__item__inner-element",
+			className: "images__container__item__img",
+			width: "200",
+			height: "200",
+			alt: "Imagen de Herbert Marcuse y Erich Fromm",
+		},
+		method: {
+			content:
+				"Generalmente es el método dialéctico, pues esta teoría deriva del marxismo, mismo que a su vez deriva de la dialéctica hegeliana.",
+			id: "method-4",
+			className: "methods__container__item__text",
+		},
+		thesis: {
+			content:
+				"Indisoluble unidad de teoría y práctica. No hay razón para el neutralismo. Ideal emancipatorio del conocimiento. Analiza la realidad social desde la categoría de totalidad.",
+			id: "thesis-4",
+			className: "thesis__container__item__text",
+		},
+		mainId: "4",
+	},
+	{
+		name: "Estructuralismo",
+		concept: {
+			name: "Es más bien una tendencia metodológica que se manifiesta hoy en muchos campos del saber y que pueden tener relaciones más o menos estrechas con distintas orientaciones filosóficas. Obedece a la exigencia de hallar una mediación entre el orden y el desorden, esto es, reducir la causalidad de los fenómenos que se presentan en un determinado campo (o en varios campos) de investigación o de experiencia a un orden relativamente constante que muestre sus relaciones reciprocas y haga posible su explicación y previsión probable.",
+			id: "concept-5",
+			className: "concepts__container__item__text",
+		},
+		image: {
+			src: "/assets/images/saussure-foucault.png",
+			id: "image-5",
+			containerClassName: "images__container__item__inner-element",
+			className: "images__container__item__img",
+			width: "200",
+			height: "200",
+			alt: "Imagen de Ferdinand de Saussure y Michel Foucault",
+		},
+		method: {
+			content:
+				"En un inicio, el método es la dialéctica orientada al análisis del lenguaje, después ser convierte en diferentes áreas como la sociología, la música, etc.",
+			id: "method-5",
+			className: "methods__container__item__text",
+		},
+		thesis: {
+			content:
+				"Estructura vs solipsismo. Descentralización del individuo, la humanidad se la da la comunidad. La cosa objeto de examen o análisis cobra sentido si se analiza dentro del sistema o estructura del que forma parte. La estructura podría definirse como un sistema de elementos interrelacionados y que constituye el modelo de explicación y formacion de la realidad.",
+			id: "thesis-5",
+			className: "thesis__container__item__text",
+		},
+		mainId: "5",
+	},
+	{
+		name: "Filosofía posmoderna",
+		concept: {
+			name: "Puede ser vista como parte de la modernidad en crisis o como un movimiento diferente, como reacción en contra de la modernidad. Tal vez, no sabemos todavía que es, por que como solo ha existido como movimiento más o menos 20 años.",
+			id: "concept-6",
+			className: "concepts__container__item__text",
+		},
+		image: {
+			src: "/assets/images/lyotard-derrida.png",
+			id: "image-6",
+			containerClassName: "images__container__item__inner-element",
+			className: "images__container__item__img",
+			width: "200",
+			height: "200",
+			alt: "Jean Francois Lyotard y Jacques Derrida",
+		},
+		method: {
+			content:
+				"En síntesis se podría decir que es una crítica a la razón, por lo tanto, el método empleado sería el racionalismo.",
+			id: "method-6",
+			className: "methods__container__item__text",
+		},
+		thesis: {
+			content:
+				"Creacionismo: No servirse de interpretaciones anteriores, el creador del sentido es el lector. Hermenéutica de la sospecha. La afirmación del sujeto. Como consecuencia de la afirmación del sujeto, existe una diversidad de fundamentos éticos.",
+			id: "thesis-6",
+			className: "thesis__container__item__text",
+		},
+		mainId: "6",
+	},
+	{
+		name: "Neoempirismo",
+		concept: {
+			name: "Se puede agrupar todas aquellas filosofías que entienden y practican la filosofía como análisis del lenguaje, esto hace referencia a dos cosas. El análisis del lenguaje científico, es decir, de los lenguajes propios de cada una de las ciencias. Hace privilegio de la ciencia y la considera como el único conocimiento valido. El análisis del lenguaje común, es decir, de las formas de expresión propias del sentido común y usadas en la vida cotidiana.",
+			id: "concept-7",
+			className: "concepts__container__item__text",
+		},
+		image: {
+			src: "/assets/images/schlick-reichenbach.png",
+			id: "image-7",
+			containerClassName: "images__container__item__inner-element",
+			className: "images__container__item__img",
+			width: "200",
+			height: "200",
+			alt: "Imagen de Moritz Schlick y Hans Reichenbach",
+		},
+		method: {
+			content: "Análisis lógico del lenguaje",
+			id: "method-7",
+			className: "methods__container__item__text",
+		},
+		thesis: {
+			content: "Neoempirismo",
+			id: "thesis-7",
+			className: "thesis__container__item__text",
+		},
+		mainId: "7",
+	},
+	{
+		name: "Filosofía de la liberación o latinoamérica",
+		concept: {
+			name: "Hace referencia a un proyecto filosófico que propugna por una contextualización de la filosofía en el ámbito latinoamericano y no, como podría inferirse, al conjunto amplio de corrientes filosóficas practicadas en los distintos países de América Latina. Es el proyecto de una filosofía surgida desde América Latina y enfocada en la reflexión sistemática sobre sus problemas y situaciones propias.",
+			id: "concept-8",
+			className: "concepts__container__item__text",
+		},
+		image: {
+			src: "/assets/images/zea-dussel.png",
+			id: "image-8",
+			containerClassName: "images__container__item__inner-element",
+			className: "images__container__item__img",
+			width: "200",
+			height: "200",
+			alt: "Imagen de Leopoldo Zea y Enrique Dussel",
+		},
+		method: {
+			content:
+				"Analéctico. Dialogo con la dialéctica hegeliana. La otredad como el reconocimiento del pensamiento latino americano a su historia",
+			id: "method-8",
+			className: "methods__container__item__text",
+		},
+		thesis: {
+			content:
+				"La liberación de las leyes del mercado, es una liberación universal de los pobres del mundo. No parte del yo pienso, sino de los pobre y oprimidos. La praxis de la liberación toma en serio los condicionamientos epistemológicos y políticos del pensar mismo. Retoma el pensar del pobre y del indígena. Retoma la historia de las ideas en América latina.",
+			id: "thesis-8",
+			className: "thesis__container__item__text",
+		},
+		mainId: "8",
+	},
+];
